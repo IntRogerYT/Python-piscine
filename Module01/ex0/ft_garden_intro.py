0248cca5ee93edd6434545df663b3659679f2437
cfop478 <rogercampsviader@gmail.com>
2026-01-31 08:04:32 +0100

    Reordering repo

--- Module01/ex0/ft_garden_intro.py
+++
@@ -1,6 +0,0 @@
-if __name__ == "__main__":
-    print("=== Welcome to My Garden ===")
-    print("Plant: Rose")
-    print("Height: 25cm")
-    print("Age: 30 days")
-    print("\n=== End of Program ===")
